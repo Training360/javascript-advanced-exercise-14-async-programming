f143d8c477fe8367f85b4f1f4052dcc8afad1d6f
Pryann <pryann@gmail.com>
2022-08-26 13:46:07 +0200

    update

--- 14-async-programming/exercises/01/solution/js/store/users.js
+++ 14-async-programming/exercises/01/solution/js/store/users.js
@@ -1,26 +1,12 @@
-/**
- * The state of users
- * @constant
- * @type {Object}
- * @property {Object[]} users - the users array
- * @default
-*/
 const state = {
   users: [],
 };
 
-/**
- * The actions of users
- * @constant
- * @type {Object}
- * @property {function} setUsers  - add new users to the users array
- * @property {function} logUsers  - log users to the console
-*/
 const actions = {
-  setUsers(users) {
+  setUsers(responseText) {
     state.users = [
       ...state.users,
-      ...users,
+      ...JSON.parse(responseText).users,
     ];
   },
   logUsers() {
